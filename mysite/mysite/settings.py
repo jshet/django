01ab6d6cf7149d37ed4e64e4abcc338b2489680d
Joel Shetler <joel.shetler@gmail.com>
2022-11-16 00:00:37 -0600

    fix debug env

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+if os.getenv("DEBUG") == 'True':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     'jshet.pythonanywhere.com',
